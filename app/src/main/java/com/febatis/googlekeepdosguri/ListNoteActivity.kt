47207207bf019d820675bce6a3a6f938fef6a8e3
Lucas Febatis <lfebatis@gmail.com>
2024-03-09 17:32:56 -0300

    Add logic with mock data on ListNoteActivity

--- app/src/main/java/com/febatis/googlekeepdosguri/ListNoteActivity.kt
+++ app/src/main/java/com/febatis/googlekeepdosguri/ListNoteActivity.kt
@@ -2,6 +2,7 @@ package com.febatis.googlekeepdosguri
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.febatis.googlekeepdosguri.databinding.ActivityListNoteBinding
 
 class ListNoteActivity : AppCompatActivity() {
@@ -11,5 +12,17 @@ class ListNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val dataset = arrayOf("January", "February", "March", "February", "March", "February", "March", "February", "March", "February", "March")
+        val noteAdapter = NoteAdapter(dataset)
+
+        binding.rvList.apply {
+            layoutManager = LinearLayoutManager(this@ListNoteActivity)
+            adapter = noteAdapter
+        }
     }
 }
---
+++ app/src/main/java/com/febatis/googlekeepdosguri/NoteAdapter.kt
@@ -0,0 +1,28 @@
+package com.febatis.googlekeepdosguri
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.febatis.googlekeepdosguri.databinding.ItemNoteBinding
+
+
+class NoteAdapter(private val dataSet: Array<String>) :
+    RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(viewGroup.context)
+        val binding = ItemNoteBinding.inflate(inflater, viewGroup, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.binding.tvTitle.text = dataSet[position]
+        viewHolder.binding.tvContent.text = dataSet[position]
+    }
+
+    override fun getItemCount() = dataSet.size
+
+}
